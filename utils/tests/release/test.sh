02135f39c26c336c97c3947695ee5e596652ae7e
Satek <filip.satek@firma.seznam.cz>
2019-03-02 12:08:04 +0100

    Fix prerelease test exit code on failure

--- utils/tests/release/test.sh
+++ utils/tests/release/test.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+# #!/bin/bash
 
-TARGET_WEB_URL="http://localhost:3000/"
+TARGET_WEB_URL="http://localhost:3000"
 NPM_LOCAL_REGISTRY_URL_NO_PROTOCOL="localhost:4873"
 NPM_LOCAL_REGISTRY_URL="http://${NPM_LOCAL_REGISTRY_URL_NO_PROTOCOL}/"
-NPM_ORIGINAL_REGISTRY=`npm config get registry`
 
 PACKAGE_VERSION=`cat package.json | grep \"version\" | head -1 | cut -d':' -f2 | cut -d'"' -f2`-next
 PACKAGE_NAME=`cat package.json | grep \"name\" | head -1 | cut -d':' -f2 | cut -d'"' -f2`
@@ -36,20 +35,35 @@ STATUS=0
 
 echo "Test1: QApe can pass the run when there are no error at the page."
 node_modules/.bin/qape -u "$TARGET_WEB_URL/noerror.html"
-[ "$?" = "0" ] && echo "pass" || (echo "fail" && STATUS=1)
+if [ "$?" = "0" ] ; then
+	echo "pass"
+else
+	echo "fail"
+	STATUS=1
+fi
 
 echo "Test2: QApe can find an error and minify the scenario."
 node_modules/.bin/qape -u "$TARGET_WEB_URL/error.html"
-[ "$?" = "1" ] && ls report/*-minified.json && echo "pass" || (echo "fail" && STATUS=1)
+if [ "$?" = "1" ] && ls report/*-minified.json ; then
+	echo "pass"
+else
+	echo "fail"
+	STATUS=1
+fi
 
 echo "Test3: QApe can replay the failing scenario."
 mv report scenarios
 node_modules/.bin/qape -u "$TARGET_WEB_URL/error.html" --random-scenarios-disabled scenarios/*-minified.json
-[ "$?" = "1" ] && ls report/*-minified.json && echo "pass" || (echo "fail" && STATUS=1)
+if [ "$?" = "1" ] && ls report/*-minified.json ; then
+	echo "pass"
+else
+	echo "fail"
+	STATUS=1
+fi
 
 # Cleanup
-kill $NPM_LOCAL_REGISTRY_PID || "Could not kill local registry"
-kill $SERVER_PID || "Could not kill server"
+kill $NPM_LOCAL_REGISTRY_PID || echo "Could not kill local registry"
+kill $SERVER_PID || echo "Could not kill server"
 
 # Send proper exit code
 exit $STATUS
