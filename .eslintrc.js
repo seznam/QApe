692e6327c649d100ea14f7c7fc84e6fcb5f39b3f
Filipoliko <satek.filip@gmail.com>
2021-03-18 09:53:54 +0100

    Removed unnecessary config options for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'extends': ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'prettier/react'],
+  'extends': ['eslint:recommended', 'prettier'],
   'parser': 'babel-eslint',
   'rules': {
     'prettier/prettier': [
@@ -8,47 +8,18 @@ module.exports = {
         semi: true,
         tabWidth: 4,
         printWidth: 110,
-        trailingComma: 'es5',
-        jsxBracketSameLine: true
+        trailingComma: 'es5'
       }
     ],
-
-    'no-console': 0,
-
-    'react/prop-types': 0,
-    'react/wrap-multilines': 0,
-    'react/display-name': 0,
   },
   'plugins': [
     'prettier',
-    'jest',
-    'react',
-    'jasmine'
+    'jest'
   ],
-  'settings': {
-    'ecmascript': 2015,
-    'jsx': true,
-    'react': {
-      version: '16'
-    }
-  },
-  'parserOptions': {
-    'sourceType': 'module',
-    'ecmaVersion': 6,
-    'ecmaFeatures': {
-      'jsx': true
-    }
-  },
   'env': {
     'browser': true,
     'node': true,
     'es6': true,
-    'jasmine': true,
     'jest/globals': true
-  },
-  'globals': {
-    '$Debug': true,
-    '$IMA': true,
-    'jsdom': true
   }
 };
